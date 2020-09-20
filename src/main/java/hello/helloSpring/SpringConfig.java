8ccfb49df2b507b85e3b34d41cc5484c0b3df92d
chateau85 <545i@naver.com>
2020-09-20 14:15:32 +0900

    [20200920] 2nd Commit of Project - 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술 - 회원 관리 예제 - 백엔드 개발, 스프링 빈과 의존관계, 화면 관리 예제 - 웹 MVC개발, 스프링 DB 접근 기술

---
+++ src/main/java/hello/helloSpring/SpringConfig.java
@@ -0,0 +1,33 @@
+package hello.helloSpring;
+
+import hello.helloSpring.repository.JpaMemberRepository;
+import hello.helloSpring.repository.MemberRepository;
+import hello.helloSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    //private EntityManager em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    //@Bean
+    //public MemberRepository memberRepository() {
+    //return new MemoryMemberRepository();
+    //return new JdbcMemberRepository(dataSource);
+    //return new JdbcTemplateMemberRepository(dataSource);
+    //return new JpaMemberRepository(em);
+    //}
+}
